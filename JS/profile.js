95b1cae0dac02e48221c7400984815e17c1c6d26
Dilnath225 <sunerarathnayaka@gmail.com>
2025-03-08 22:39:15 +0530

    profile.js

---
+++ JS/profile.js
@@ -0,0 +1,15 @@
+
+    // Handle the image selection
+    const fileInput = document.getElementById('fileInput');
+    const profilePicture = document.getElementById('profile-picture');
+
+    fileInput.addEventListener('change', function(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+          profilePicture.src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+      }
+    });
